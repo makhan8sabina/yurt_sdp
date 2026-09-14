d2f3d2ad2c532b5f9a3b8437520de78e393d2b96
makhan8sabina <makhan.sabina@gmail.com>
2026-09-14 11:43:40 +0500

    Update YurtDirector.java

--- YurtDirector.java
+++ YurtDirector.java
@@ -1,43 +1,24 @@
+package yurtbuilder;
+
 public class YurtDirector {
 
-    public Yurt constructTraditionalNomadicYurt(YurtBuilder builder) {
+    public Yurt constructNomadicYurt(YurtBuilder builder) {
         return builder
-                .setFrameType(YurtConstants.FRAME_WOODEN_LATTICE)
-                .setDiameter(YurtConstants.DEFAULT_NOMADIC_DIAMETER)
-                .setCovering(YurtConstants.COVERING_FELT)
-                .setDoorDirection(YurtConstants.DOOR_SOUTH_EAST)
+                .setFrameType("wooden lattice frame")
+                .setDiameter(6)
+                .setCovering("felt")
                 .installStove(true)
-                .addDecoration("shanyrak (crown wheel) left undecorated, as tradition")
-                .addDecoration("hand-woven ornamented bands (bau)")
-                .setPurpose(YurtPurpose.NOMADIC)
+                .setPurpose("NOMADIC")
                 .build();
     }
 
-    public Yurt constructTouristGlampingYurt(YurtBuilder builder) {
+    public Yurt constructTouristYurt(YurtBuilder builder) {
         return builder
-                .setFrameType(YurtConstants.FRAME_STEEL_COLLAPSIBLE)
-                .setDiameter(YurtConstants.DEFAULT_TOURIST_DIAMETER)
-                .setCovering(YurtConstants.COVERING_CANVAS)
-                .setDoorDirection(YurtConstants.DOOR_GLASS)
+                .setFrameType("steel frame")
+                .setDiameter(5)
+                .setCovering("canvas")
                 .installStove(false)
-                .addDecoration("string lighting")
-                .addDecoration("panoramic roof window")
-                .setPurpose(YurtPurpose.TOURIST_GLAMPING)
-                .build();
-    }
-
-    public Yurt constructCeremonialWeddingYurt(YurtBuilder builder) {
-        return builder
-                .setFrameType(YurtConstants.FRAME_WOODEN_LATTICE)
-                .setDiameter(YurtConstants.DEFAULT_CEREMONIAL_DIAMETER)
-                .setCovering(YurtConstants.COVERING_DECORATIVE_SILK)
-                .setDoorDirection(YurtConstants.DOOR_SOUTH_EAST)
-                .installStove(true)
-                .addDecoration("gold-embroidered wall hangings (tuskiyz)")
-                .addDecoration("floral shyrdak floor carpets")
-                .addDecoration("ceremonial entrance arch")
-                .setPurpose(YurtPurpose.CEREMONIAL)
+                .setPurpose("TOURIST")
                 .build();
     }
 }
-
